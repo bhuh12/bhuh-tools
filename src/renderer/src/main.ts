d87deace781a3bfd0c9d9f2c47060dc770cbe7b1
bhuh12 <bhuh12@126.com>
2025-07-26 23:17:12 +0800

    feat: 添加 `Color4Bg` 动态背景

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -2,6 +2,7 @@ import '@/assets/css/main.scss'
 
 import { createApp } from 'vue'
 import router from './plugins/router'
+import './plugins/color4Bg'
 import App from './App.vue'
 
 createApp(App).use(router).mount('#app')
---
+++ src/renderer/src/plugins/color4Bg.ts
@@ -0,0 +1,10 @@
+/**
+ * 初始化动态背景
+ * @link https://color4bg.com/zh-hans/generator/vivid-blue-pink-fluid-background
+ */
+//@ts-ignore index.html 中已引入
+new Color4Bg.AestheticFluidBg({
+  dom: 'app-bg',
+  colors: ['#59E2FD', '#6C58EE', '#F54595', '#FCE500', '#59E2FD', '#6C58EE'],
+  loop: true
+})
